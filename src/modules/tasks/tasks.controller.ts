1f57e747f54ba49a390d54b20191ac1135f2b2d5
Parsa <mparsabahredar@gmail.com>
2026-08-28 07:48:23 +0330

    Fix bugs.

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -30,65 +30,68 @@ export const getTasks = (req: Request, res: Response) => {
   res.status(200).json(tasks);
 };
 
+
 export const getTask = (req: Request, res: Response) => {
   const id = Number(req.params.id);
-
   const task = getTaskById(id);
-
   if (!task) {
     return res.status(404).json({
       message: "Task not found",
     });
   }
-
   res.status(200).json(task);
 };
 
+
 export const addTask = (req: Request, res: Response) => {
   const { title, attachmentPath } = req.body;
-
+  if (!title) {
+    return res.status(400).json({
+      message: "Title is required",
+    });
+  }
   const task = createTask(
     title,
     attachmentPath ?? null
   );
-
   res.status(201).json(task);
 };
 
+
 export const editTask = (req: Request, res: Response) => {
   const id = Number(req.params.id);
-
   const { title, completed, attachmentPath } = req.body;
-
+  if (title === "") {
+    return res.status(400).json({
+      message: "Title cannot be empty",
+    });
+  }
   const task = updateTask(
     id,
     title,
     completed,
     attachmentPath
   );
-
   if (!task) {
     return res.status(404).json({
       message: "Task not found",
     });
   }
-
   res.status(200).json(task);
 };
+
 
 export const toggleTaskStatus = (req: Request, res: Response) => {
   const id = Number(req.params.id);
-
   const task = toggleTask(id);
-
   if (!task) {
     return res.status(404).json({
       message: "Task not found",
     });
   }
-
   res.status(200).json(task);
 };
+
 
 export const removeTask = (req: Request, res: Response) => {
   const id = Number(req.params.id);
